b289cdf549c86d0dac733824541bd6882c6c7598
gq <guanqinghz@163.com>
2019-07-17 22:47:57 +0800

    Vuex之state,getters,mutation,action等

--- client/app.vue
+++ client/app.vue
@@ -4,7 +4,9 @@
         <Header></Header>
         <!--<router-link :to="{name: 'app'}">app</router-link>-->
         <!--<router-link to="/app/123">app</router-link>-->
-        <p>{{count}}</p>
+        <!--<p>{{count}}</p>-->
+        <p>{{counter}}</p>
+        <p>{{fullName}}</p>
         <router-link to="/slide">slide</router-link>
         <router-link to="/app/123">app123</router-link>
         <router-link to="/app/456">app456</router-link>
@@ -19,32 +21,72 @@
 </template>
 
 <script>
-    import Header from './layout/header.vue'
-    import Footer from './layout/footer.jsx'
-    // import Todo from './views/todo/todo.vue'
+import {
+  mapState,
+  mapGetters,
+  mapActions,
+  mapMutations
+} from 'vuex'
+import Header from './layout/header.vue'
+import Footer from './layout/footer.jsx'
 
-    // console.log(Header.__docs)
-
-    export default {
-      components: {
-        Header,
-        Footer
-        // Todo
-      },
-      mounted () {
-        // console.log(this.$route)
-        console.log(this.$store)
-        let i = 1
-        setInterval(() => {
-          this.$store.commit('updateCount', i++)
-        }, 1000)
-      },
-      computed: {
-        count () {
-          return this.$store.state.count
-        }
-      }
-    }
+// import Todo from './views/todo/todo.vue'
+// console.log(Header.__docs)
+export default {
+  components: {
+    Header,
+    Footer
+    // Todo
+  },
+  mounted () {
+    // console.log(this.$route)
+    console.log(this.$store)
+    // this.$store.dispatch('updateCountAsync', {
+    //   num: 5,
+    //   time: 2000
+    // })
+    // this.$store.state.count = 3 可以改成功,但不建议这么改 strict: isDev 规范数据操作
+    // let i = 1
+    // setInterval(() => {
+    //   this.$store.commit('updateCount', i++)
+    // }, 1000)
+    // setInterval(() => {
+    //   this.$store.commit('updateCount', {
+    //     num: i++,
+    //     num2: 2
+    //   })
+    // }, 1000)
+    this.updateCountAsync({
+      num: 5,
+      time: 2000
+    })
+    let i = 1
+    setInterval(() => {
+      this.updateCount({
+        num: i++,
+        num2: 2
+      })
+    }, 1000)
+  },
+  methods: {
+    ...mapActions(['updateCountAsync']),
+    ...mapMutations(['updateCount'])
+  },
+  computed: {
+    // ...mapState(['count']),
+    ...mapState({
+      // counter: 'count'
+      counter: (state) => state.count
+    }),
+    // count () {
+    //   return this.$store.state.count
+    // },
+    ...mapGetters(['fullName'])
+    // fullName () {
+    //   return this.$store.getters.fullName
+    // }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
--- client/store/store.js
+++ client/store/store.js
@@ -1,14 +1,18 @@
 import Vuex from 'vuex'
 
+import defaultState from './state/state'
+import mutations from './mutations/mutation'
+import getters from './getters/getters'
+import actions from './actions/actions'
+
+const isDev = process.env.NODE_ENV === 'development'
+
 export default () => {
   return new Vuex.Store({
-    state: {
-      count: 0
-    },
-    mutations: {
-      updateCount (state, num) {
-        state.count = num
-      }
-    }
+    strict: isDev, // 开发的时候可以打开,规范开发人员操作store
+    state: defaultState,
+    mutations,
+    getters,
+    actions
   })
 }
